344b3e94e63bcb49e358cc7f8c38fb608557a09c
Toxic09-g <roma1605@mail.ru>
2024-06-15 18:28:22 +0500

    fix

---
+++ src/api/users.js
@@ -0,0 +1,5 @@
+import data from '../data/users.json' 
+const getUsers = () =>{
+    return data
+}
+export default getUsers
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,47 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../App.css"
 import { SidebarData } from './SidebarData'
 import Avatar from '../icons/Ellipse 6.png'
 
 function Sidebar() {
+    const [sidebar, setSidebar] = useState(false);
+
+    const toggleSidebar = () => {
+        setSidebar(!sidebar);
+    };
 
     return (
-        <div className='Sidebar'>
-            <div className='Toogle'>
-                <span><svg width="20" height="14" viewBox="0 0 20 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="20" height="2" fill="#9494A0" />
-                    <rect y="6" width="20" height="2" fill="#9494A0" />
-                    <rect y="12" width="20" height="2" fill="#9494A0" />
-                </svg>
+        <div>
+            <div className='Toggle' onClick={toggleSidebar}>
+                <span>
+                    <svg width="20" height="14" viewBox="0 0 20 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="20" height="2" fill="#9494A0" />
+                        <rect y="6" width="20" height="2" fill="#9494A0" />
+                        <rect y="12" width="20" height="2" fill="#9494A0" />
+                    </svg>
                 </span>
+                <h1>Права доступа</h1>
             </div>
-            <div className='Logo'>
-            </div>
-            <div className='Profile'>
-                <div className='Avatar'>
-                    <img src={Avatar} alt='' />
-                </div>
-                <div className='Info'>
-                    <div className='Name'>
-                        Артем Иванов
+            <div className={sidebar ? 'Sidebar active' : 'Sidebar'}>
+                
+                <div className='Logo'></div>
+                <div className='Profile'>
+                    <div className='Avatar'>
+                        <img src={Avatar} alt='' />
                     </div>
-                    <div className='Role'>
-                        Собственник
+                    <div className='Info'>
+                        <div className='Name'>
+                            Артем Иванов
+                        </div>
+                        <div className='Role'>
+                            Собственник
+                        </div>
                     </div>
                 </div>
-            </div>
-            <ul>
-                {SidebarData.map((val, key) => {
-                    return (
+                <ul>
+                    {SidebarData.map((val, key) => (
                         <li key={key} onClick={() => window.location.pathname = val.link}>
                             <div className='Icon'>{val.icon}</div>
                             <div className='Title'>{val.title}</div>
                         </li>
-                    )
-                })}
-            </ul>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
 
-export default Sidebar
+export default Sidebar;
